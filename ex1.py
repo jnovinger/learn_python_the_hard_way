21be0363bfd63d04cc10db0148636213734d3bd1
Jason Novinger <jnovinger@gmail.com>
2011-06-27 19:17:07 -0500

    Exercise 1

---
+++ ex1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''
+
+'''
+
+print "Hello World!"
+print "Hell Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# extra credit stuff
+print "Here is another line!"
