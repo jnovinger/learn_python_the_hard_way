79030b2ee4816b23070136915cef8b10fb741d1d
Jason Novinger <jnovinger@gmail.com>
2011-06-27 20:00:36 -0500

    Exercise 6

---
+++ ex6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+x = "There are %d type of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left sife of..."
+e = "a string with a right side."
+
+print w + e
