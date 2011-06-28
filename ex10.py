da47c6843f3703137d429893961d37af72abd88a
Jason Novinger <jnovinger@gmail.com>
2011-06-27 20:23:36 -0500

    Exercise 10 extra credit

--- ex10.py
+++ ex10.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+tabby_cat = "%sI'm tabbed in." % '\t'
+persian_cat = "I'm split%son a line." % '\n'
+backslash_cat = "I'm %s a %s cat." % ('\\', '\\')
 
-fat_cat = """
+r_formatter = "%r %r" % (tabby_cat, persian_cat)
+s_formatter = "%s %s" % (tabby_cat, persian_cat)
+
+print r_formatter
+print s_formatter
+
+fat_cat = '''
 I'll do a list:
 \t* Cat Food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print tabby_cat
 print persian_cat
