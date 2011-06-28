9beeae5b80c81334fa1dcfcfc0555250a87ba0ef
Jason Novinger <jnovinger@gmail.com>
2011-06-27 19:31:33 -0500

    Exercise 3

---
+++ ex3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+'''
+
+'''
+
+# print stmt
+print "I will now count my chickens:"
+
+# print number of hens
+print "Hens", 25 + 30 / 6
+# print number of roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+#print stmt
+print "Now I will count the eggs:"
+
+# print number of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# print a stmt
+print "Is it true that 3 + 2 < 5 - 7?"
+# evaluate and print a boolean
+print 3 + 2 < 5 - 7
+
+# print what 3 + 2 is
+print "What is 3 + 2?", 3 + 2
+# print what 5 - 7 is
+print "What is 5 - 7?", 5 - 7
+
+# print stmt
+print "Oh, that's why it's False."
+
+# print stmt
+print "How about some more."
+
+#print some more math
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
