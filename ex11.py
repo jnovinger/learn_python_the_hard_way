4623bccc594acfe55415b0c7706520a2215717b7
Jason Novinger <jnovinger@gmail.com>
2011-06-27 20:35:09 -0500

    Exercise 11 extra credit

--- ex11.py
+++ ex11.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 print "How old are you?",
-age = raw_input()
+age = raw_input('(in years) --> ')
 print "How tall are you?",
-height = raw_input()
+height = raw_input('(in feet an inches) --> ')
 print "How much do you weigh?",
-weight = raw_input()
+weight = raw_input('(in lbs) --> ')
 
-print "So, you're %r old, %r tall, and %r heavy." % (
+print "So, you're %s years old, %s inches tall, and %s lbs heavy." % (
     age, height, weight)
+
+# BMI = (weight in lbs * 703) / (height in inches)^2
+bmi = (float(weight) * 703) / (float(height) ** 2)
+print "And your BMI is %f." % bmi
