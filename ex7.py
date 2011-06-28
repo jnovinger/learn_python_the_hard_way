6f0b7c1aef5c6c3fa0e164d041cb52a90c4324ee
Jason Novinger <jnovinger@gmail.com>
2011-06-27 20:08:19 -0500

    Exercise 7 and extra credit

---
+++ ex7.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# print out several strings
+print "Mary had a little lamb."
+# including a formatted string
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+# and a funky string concatenation
+print "." * 10
+
+# define a bunch of single char strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# then append them and print them out
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
