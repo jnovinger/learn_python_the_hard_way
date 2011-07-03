00a232f47cc91a0fa66beb0ec5171f47d998b74d
Jason Novinger <jnovinger@gmail.com>
2011-07-03 00:32:11 -0500

    Exercise 15 extra credit

--- ex15.py
+++ ex15.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
+# import argv module
 from sys import argv
 
+# unpack argv and assign to vars
 script, filename = argv
 
+# open txt file for reading
 txt = open(filename)
 
+# echo back filename
 print "Here's your file %r:" % filename
+# echo back contents of the txt file
 print txt.read()
 
-print "I'll also ask you to type it again:"
-file_again = raw_input("> ")
-
-txt_again = open(file_again)
-
-print txt_again.read()
+## prompt user for the filename again
+#print "I'll also ask you to type it again:"
+#file_again = raw_input("> ")
+#
+## open the file again for reading
+#txt_again = open(file_again)
+#
+## echo the file again
+#print txt_again.read()
