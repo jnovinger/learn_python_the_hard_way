f56b00d75f63272a21994e7f04a392f418914494
Jason Novinger <jnovinger@gmail.com>
2011-07-03 00:03:51 -0500

    Exercise 13

---
+++ ex13.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+LPTHW Exercise 13
+'''
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "The first variable is:", first
+print "The second variable is:", second
+print "The third variable is:", third
