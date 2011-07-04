3dbb5fb4f2bbdf514725070ed5492684e39a3fcd
Jason Novinger <jnovinger@gmail.com>
2011-07-03 20:10:04 -0500

    Exercise 16 extra credit

--- ex16.py
+++ ex16.py
@@ -28,12 +28,9 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+output = "%s\n%s\n%s\n" % (line1, line2, line3)
+
+target.write(output)
 
 print "And finally, we close it."
 target.close()
