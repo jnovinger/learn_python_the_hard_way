9f2dcac595c4dd85be0ceee783034e2c21fa0b89
Jason Novinger <jnovinger@gmail.com>
2011-07-03 22:25:05 -0500

    Exercise 33 extra credit #1

--- ex33.py
+++ ex33.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-    i += 1
-    print "Numbers now:", numbers
-    print "At the bottom i is %d" % i
+def populate_numbers(numbers):
+    i = 0
+    limit = 6
+    while i < limit:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        i += 1
+        print "Numbers now:", numbers
+        print "At the bottom i is %d" % i
+    return numbers
+
+numbers = populate_numbers(numbers)
 
 print "The numbers: "
 
