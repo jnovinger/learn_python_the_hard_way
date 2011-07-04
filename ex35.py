f0aaaa9b257a435c48e27589ad971dcc5100286b
Jason Novinger <jnovinger@gmail.com>
2011-07-03 23:50:15 -0500

    Exercise 35 extra credit

--- ex35.py
+++ ex35.py
@@ -5,11 +5,7 @@ from sys import exit
 def gold_room():
     print "This room is full of gold. How much do you take?"
 
-    next = raw_input("> ")
-    if "0" in next or "1" in next:
-        how_much = int(next)
-    else:
-        dead("Man, learn to type a number.")
+    how_much = int(raw_input("> "))
 
     if how_much < 50:
         print "Nice, you're not greedy, you win!"
