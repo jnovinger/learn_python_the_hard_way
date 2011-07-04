47aaba491540ecf85beca093021f8ecd02118a54
Jason Novinger <jnovinger@gmail.com>
2011-07-03 22:28:20 -0500

    Exercise 33 extra credit #3

--- ex33.py
+++ ex33.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
-def populate_numbers(numbers, start, stop):
+def populate_numbers(numbers, start, stop, increment = 1):
     while start < stop:
         print "At the top i is %d" % start
         numbers.append(start)
-        start += 1
+        start += increment
         print "Numbers now:", numbers
         print "At the bottom i is %d" % start
     return numbers
 
 start = 0
 stop = 6
+increment = 2
 numbers = []
-numbers = populate_numbers(numbers, start, stop)
+numbers = populate_numbers(numbers, start, stop, increment)
 
 print "The numbers: "
 
